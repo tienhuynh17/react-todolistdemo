277dd9c826798945205f06532ffd8935a59b0dee
tienzlatan <39383420+tienzlatan@users.noreply.github.com>
2020-06-10 23:08:19 +0700

    Updates

---
+++ precache-manifest.ba1121b9b99cbf70cc04efcde032d476.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c27730a7ae9f24ea74728048023298d6",
+    "url": "/react-todolistdemo/index.html"
+  },
+  {
+    "revision": "1e1be048f5da61121146",
+    "url": "/react-todolistdemo/static/css/main.83bbf3bb.chunk.css"
+  },
+  {
+    "revision": "68b2cb9854fd14a44d61",
+    "url": "/react-todolistdemo/static/js/2.d9ca47ad.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todolistdemo/static/js/2.d9ca47ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e1be048f5da61121146",
+    "url": "/react-todolistdemo/static/js/main.66f723fa.chunk.js"
+  },
+  {
+    "revision": "8f0467663d8aa0ac77af",
+    "url": "/react-todolistdemo/static/js/runtime-main.5bfae53b.js"
+  }
+]);
